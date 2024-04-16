38c65fb46016d6d5017d40eec27a2e4a7e5a4196
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-04-17 01:17:28 +0300

    upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,23 +5,6 @@ import { Notification } from './Notification';
 import { GlobalStyles } from './GlobalStyle';
 import { AppWraper } from './App.styled';
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
-
 export class App extends Component {
   state = {
     good: 0,
